d07559753e912145c99043595a30451a5fea4233
Claude.Sczygiol <claude.sczygiol@prosiebensat1.com>
2023-11-01 17:03:26 +0100

    adding files

--- tests/yt_tests.spec.ts
+++ tests/yt_tests.spec.ts
@@ -4,9 +4,7 @@ import { yt_tasks } from "../screenplay/tasks/yt_tasks";
 test.describe("test", () => {
     test("Login", async ({ User }) => {
         await User.attemptsTo(
-            await User.attemptsTo(
-                yt_tasks.Login(),
-            )
+            yt_tasks.Login(),
         );
     })
 })
